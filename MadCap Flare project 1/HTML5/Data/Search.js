17c1edb856ac135fb5d06b35e7a17c5aebbbd0cf
ddyszkiewicz <dyszkiewicz.dariusz@gmail.com>
2023-06-04 16:49:14 +0200

    MadCap Flare project 1

---
+++ MadCap Flare project 1/HTML5/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Medicine/Electronic Health Records.htm'],s:['2002'],p:[['2002',1],['machines',12]]});
---
+++ MadCap Flare project 1/HTML5/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=638214921210886831'
+});
